f82772fb333ad728efd2dfdc730e2050c37c5dd7
Jose Manuel Centeno Meza <josemcentenoc@gmail.com>
2025-06-14 23:19:09 -0300

    env variables vite config

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import './index.css'
 import App from './App.jsx'
 
 Sentry.init({
-  dsn: import.meta.env.SENTRY_AUTH_TOKEN,
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
+  dsn: "https://dd3ccc5375c03291bdec6db1b3856309@o4509493397028864.ingest.us.sentry.io/4509499837382656",
   sendDefaultPii: true,
   integrations: [
+    Sentry.browserTracingIntegration(),
     Sentry.replayIntegration()
   ],
-  replaysSessionSampleRate: 0.1,
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0
 });
 
